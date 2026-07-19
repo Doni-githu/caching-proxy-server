08ac9777f3fb8698e940e5d76fe397029346f480
doni-gh01 <ddonierov01@gmail.comr>
2026-07-19 16:49:45 +0500

    added limiter to caching project for safety

--- src/server.js
+++ src/server.js
@@ -1,9 +1,26 @@
-import express, { json } from "express"
+import express from "express"
 import redis from "./redis.js"
 import axios from "axios";
 
+import {rateLimit} from "express-rate-limit"
+import RedisStore from "rate-limit-redis";
+
 
 const app = express()
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 *1000,
+    limit: 100,
+    standardHeaders: "draft-8",
+    legacyHeaders: false,
+    ipv6Subnet: 56,
+    store: new RedisStore({
+        sendCommand: (...args) => redis.call(...args),
+    }),
+    handler: (req, res) => {
+        res.status(429).json({ message: "Too many requests, please try again later." });
+    }
+})
 
 
 export default function CachingProxyServer(port, origin) {
@@ -32,6 +49,7 @@ export default function CachingProxyServer(port, origin) {
     }
 
     const startServer = () => {
+        app.use(limiter)
         app.use(handlerRequest);
         app.listen(Number(port), () => {
             console.log(`Server running on http://localhost:${port}`);
